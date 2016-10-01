9a38b863fef95de1ccdad14f0610b0c5a0e28a2f
Rohan Anand <thededlier@gmail.com>
2016-10-02 00:09:39 +0530

    Intent to send username

--- app/src/main/java/com/thededlier/rohan/bookmark/LoginActivity.java
+++ app/src/main/java/com/thededlier/rohan/bookmark/LoginActivity.java
@@ -1,13 +1,23 @@
 package com.thededlier.rohan.bookmark;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.EditText;
 
 public class LoginActivity extends AppCompatActivity {
+
+    EditText etUserName;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        etUserName = (EditText)findViewById(R.id.user_name);
+
+        Intent intent = new Intent(this, MainActivity.class);
+        intent.putExtra(Intent.EXTRA_TEXT, etUserName.getText().toString());
+        startActivity(intent);
     }
 }
