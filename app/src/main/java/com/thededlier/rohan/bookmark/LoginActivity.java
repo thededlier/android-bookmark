1a0cf7390d8a831ef3ff53c0ab3d6fa1bc62098f
Rohan Anand <thededlier@gmail.com>
2016-10-02 01:10:50 +0530

    Added continue button to go to next intent

--- app/src/main/java/com/thededlier/rohan/bookmark/LoginActivity.java
+++ app/src/main/java/com/thededlier/rohan/bookmark/LoginActivity.java
@@ -3,21 +3,31 @@ package com.thededlier.rohan.bookmark;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 
 public class LoginActivity extends AppCompatActivity {
 
     EditText etUserName;
+    Button btnSubmit;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
-        etUserName = (EditText)findViewById(R.id.user_name);
+        final Intent intent = new Intent(this, MainActivity.class);
 
-        Intent intent = new Intent(this, MainActivity.class);
-        intent.putExtra(Intent.EXTRA_TEXT, etUserName.getText().toString());
-        startActivity(intent);
+        etUserName  = (EditText)findViewById(R.id.user_name);
+        btnSubmit   = (Button)findViewById(R.id.btnSubmit);
+
+        btnSubmit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                intent.putExtra(Intent.EXTRA_TEXT, etUserName.getText().toString());
+                startActivity(intent);
+            }
+        });
     }
 }
